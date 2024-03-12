6f8934882622fdfbe24ad6cf186dd42728c82e6c
lunarW1TCH <lunarw1tch@proton.me>
2024-03-12 14:25:43 +0100

    feat: added content for lunarify and minos

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,5 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-  type: 'content',
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
-
 const project = defineCollection({
   type: 'content',
   schema: z.object({
@@ -76,4 +63,4 @@ const skill = defineCollection({
   }),
 });
 
-export const collections = { blog, project, vector, skill };
+export const collections = { project, vector, skill };
